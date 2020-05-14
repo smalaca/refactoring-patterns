67ca785eea363a2e96926d3d140323bd2ba9e5de
smalaca <ab13.krakow@gmail.com>
2020-05-14 15:33:23 +0200

    Improve tests of PullRequestApplicationService by replacing Branch mock with real object

--- src/test/java/com/smalaca/application/service/PullRequestApplicationServiceTest.java
+++ src/test/java/com/smalaca/application/service/PullRequestApplicationServiceTest.java
@@ -1,19 +1,22 @@
 package com.smalaca.application.service;
 
 import com.smalaca.application.response.Response;
-import com.smalaca.domain.*;
+import com.smalaca.domain.Branch;
+import com.smalaca.domain.BranchRepository;
+import com.smalaca.domain.PullRequest;
+import com.smalaca.domain.PullRequestRepository;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
 class PullRequestApplicationServiceTest {
     private static final String BRANCH_ID = UUID.randomUUID().toString();
     private static final String PULL_REQUEST_ID = UUID.randomUUID().toString();
+    private static final String BRANCH_NAME = UUID.randomUUID().toString();
 
     private final BranchRepository branchRepository = mock(BranchRepository.class);
     private final PullRequestRepository pullRequestRepository = mock(PullRequestRepository.class);
@@ -21,9 +24,10 @@ class PullRequestApplicationServiceTest {
 
     @Test
     void shouldReturnSuccessWhenMergeSucceed() {
-        Branch branch = mock(Branch.class);
+        Branch branch = new Branch(BRANCH_NAME);
         given(branchRepository.find(BRANCH_ID)).willReturn(branch);
         PullRequest pullRequest = mock(PullRequest.class);
+        given(pullRequest.isPossibleToMergeTo(branch)).willReturn(true);
         given(pullRequestRepository.find(PULL_REQUEST_ID)).willReturn(pullRequest);
 
         Response actual = service.merge(BRANCH_ID, PULL_REQUEST_ID);
@@ -33,11 +37,11 @@ class PullRequestApplicationServiceTest {
 
     @Test
     void shouldReturnFailureWhenMergeWasNotPossible() {
+        Branch branch = new Branch(BRANCH_NAME);
+        given(branchRepository.find(BRANCH_ID)).willReturn(branch);
         PullRequest pullRequest = mock(PullRequest.class);
         given(pullRequestRepository.find(PULL_REQUEST_ID)).willReturn(pullRequest);
-        Branch branch = mock(Branch.class);
-        given(branchRepository.find(BRANCH_ID)).willReturn(branch);
-        doThrow(new MergeException(branch, pullRequest)).when(branch).merge(pullRequest);
+        given(pullRequest.isPossibleToMergeTo(branch)).willReturn(false);
 
         Response actual = service.merge(BRANCH_ID, PULL_REQUEST_ID);
 
