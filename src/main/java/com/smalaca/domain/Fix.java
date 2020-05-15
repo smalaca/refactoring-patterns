99d0b4072c1bebd6a7ee1c401cc5924e9aa91845
smalaca <ab13.krakow@gmail.com>
2020-05-15 10:58:04 +0200

    Subclass with Fields

--- src/main/java/com/smalaca/domain/Fix.java
+++
@@ -1,9 +0,0 @@
-package com.smalaca.domain;
-
-import java.util.List;
-
-public class Fix extends PullRequest {
-    public Fix(Author author, String name, List<Commit> commits) {
-        super(author, name, commits);
-    }
-}
--- src/main/java/com/smalaca/domain/Improvement.java
+++
@@ -1,9 +0,0 @@
-package com.smalaca.domain;
-
-import java.util.List;
-
-public class Improvement extends PullRequest {
-    public Improvement(Author author, String name, List<Commit> commits) {
-        super(author, name, commits);
-    }
-}
