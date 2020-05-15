2e123aa2003d6029bb633b2df4d85fd523e1ece5
smalaca <ab13.krakow@gmail.com>
2020-05-15 12:11:11 +0200

    Collapse Hierarchy

--- src/main/java/com/smalaca/notification/NotificationSender.java
+++ src/main/java/com/smalaca/notification/NotificationSender.java
@@ -2,8 +2,38 @@ package com.smalaca.notification;
 
 import com.smalaca.domain.PullRequest;
 
-public interface NotificationSender {
-    void notify(PullRequest pullRequest);
+import java.time.LocalDate;
+import java.util.Date;
 
-    void notify(String message, String receiver);
+public class NotificationSender {
+    private final NotificationDeltaRepository repository;
+
+    public NotificationSender(NotificationDeltaRepository repository) {
+        this.repository = repository;
+    }
+
+    public void notify(PullRequest pullRequest) {
+        send(pullRequest, pullRequest.getAuthor().getLogin());
+
+        pullRequest.getReviewers().forEach(reviewer -> send(pullRequest, reviewer.getLogin()));
+    }
+
+    private void send(PullRequest pullRequest, String login) {
+        NotificationDelta delta = repository.findFor(login);
+        LocalDate delayed = getDelayedNotificationDate(pullRequest.getMergeDate(), delta.getDaysDelay());
+
+        send(pullRequest, login, delayed);
+    }
+
+    private LocalDate getDelayedNotificationDate(Date mergeDate, int daysDelay) {
+        return new DelayedNotificationDate(mergeDate, daysDelay).asLocalDate();
+    }
+
+    private void send(PullRequest pullRequest, String login, LocalDate delayed) {
+        // to implement
+    }
+
+    public void notify(String message, String receiver) {
+        // to implement
+    }
 }
