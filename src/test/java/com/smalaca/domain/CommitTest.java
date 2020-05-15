99f4b214deb9fcfd40fc94b1e8edf9f5b090e797
smalaca <ab13.krakow@gmail.com>
2020-05-15 10:07:01 +0200

    add tests of Commit

---
+++ src/test/java/com/smalaca/domain/CommitTest.java
@@ -0,0 +1,24 @@
+package com.smalaca.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import static org.mockito.Mockito.mock;
+
+class CommitTest {
+    @Test
+    void shouldReturnMessage() {
+        String hashCode = UUID.randomUUID().toString();
+        LocalDate creationDate = LocalDate.now();
+        Author author = mock(Author.class);
+        CodeBaseDelta codeBaseDelta = mock(CodeBaseDelta.class);
+        String[] message = new String[]{"number", "title"};
+
+        Commit commit = new Commit(hashCode, creationDate, author, codeBaseDelta, message);
+
+        Assertions.assertThat(commit.message()).isEqualTo("number - title");
+    }
+}
