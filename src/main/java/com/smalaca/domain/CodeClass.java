4a79780fa16ccc387b3859a8130e86a5ddbd3d91
smalaca <ab13.krakow@gmail.com>
2020-05-15 11:44:18 +0200

    Replace Nested Conditional with Guard Clauses

--- src/main/java/com/smalaca/domain/CodeClass.java
+++ src/main/java/com/smalaca/domain/CodeClass.java
@@ -23,29 +23,40 @@ public class CodeClass {
 
     public int linesOfCode() {
         if (CodeClassType.INTERFACE.equals(type)) {
-            return methods.size() + 1;
-        } else {
-            if (CodeClassType.ENUM.equals(type)) {
-                int amountOfAttributes = attributes.size();
-                int methodsLength = methods.stream().mapToInt(ClassMethod::bodySize).sum();
-
-                return amountOfAttributes + methodsLength;
-            } else {
-
-                if (parent != null) {
-                    int linesOfCode = parent.linesOfCode();
-                    int amountOfAttributes = attributes.size();
-                    int methodsLength = methods.stream().mapToInt(ClassMethod::bodySize).sum();
-
-                    return linesOfCode + amountOfAttributes + methodsLength;
-                } else {
-                    int amountOfAttributes = attributes.size();
-                    int methodsLength = methods.stream().mapToInt(ClassMethod::bodySize).sum();
-
-                    return amountOfAttributes + methodsLength;
-                }
-            }
+            return linesOfInterfaceCode();
         }
+
+        if (CodeClassType.ENUM.equals(type)) {
+            return linesOfEnumCode();
+        }
+
+        return linesOfClassCode();
+    }
+
+    private int linesOfClassCode() {
+        if (parent != null) {
+            int linesOfCode = parent.linesOfCode();
+            int amountOfAttributes = attributes.size();
+            int methodsLength = methods.stream().mapToInt(ClassMethod::bodySize).sum();
+
+            return linesOfCode + amountOfAttributes + methodsLength;
+        } else {
+            int amountOfAttributes = attributes.size();
+            int methodsLength = methods.stream().mapToInt(ClassMethod::bodySize).sum();
+
+            return amountOfAttributes + methodsLength;
+        }
+    }
+
+    private int linesOfEnumCode() {
+        int amountOfAttributes = attributes.size();
+        int methodsLength = methods.stream().mapToInt(ClassMethod::bodySize).sum();
+
+        return amountOfAttributes + methodsLength;
+    }
+
+    private int linesOfInterfaceCode() {
+        return methods.size() + 1;
     }
 
     public boolean hasMoreLinesThan(int linesOfCode) {
