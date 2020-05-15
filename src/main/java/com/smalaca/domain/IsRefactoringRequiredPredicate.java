9ecfb77eeba55610e131ace7d46103e565184bd9
smalaca <ab13.krakow@gmail.com>
2020-05-15 11:16:35 +0200

    Consolidate Conditional Expressions - extract methods

--- src/main/java/com/smalaca/domain/IsRefactoringRequiredPredicate.java
+++ src/main/java/com/smalaca/domain/IsRefactoringRequiredPredicate.java
@@ -3,19 +3,31 @@ package com.smalaca.domain;
 public class IsRefactoringRequiredPredicate {
 
     public boolean check(ChangeScope scope, CodeBase codeBase) {
-        if (scope.affectedLinesOfCode() <= 100) {
+        if (hasNotEnoughAffectedLinesOfCode(scope)) {
             return false;
         }
 
-        if (scope.numberOfCommits() / ((scope.latestChangeEpochDay() - scope.firstChangeEpochDay()) / 28) < 2) {
+        if (hasNotEnoughAverageCommitsPerMonth(scope)) {
             return false;
         }
 
-        if (scope.latestChangeEpochDay() > (codeBase.latestChangeEpochDay() - 84)) {
+        if (isLatestCommitNotOlderThanThreeMonths(scope, codeBase)) {
             return true;
         } else {
             return false;
         }
+    }
+
+    private boolean isLatestCommitNotOlderThanThreeMonths(ChangeScope scope, CodeBase codeBase) {
+        return scope.latestChangeEpochDay() > (codeBase.latestChangeEpochDay() - 84);
+    }
+
+    private boolean hasNotEnoughAverageCommitsPerMonth(ChangeScope scope) {
+        return scope.numberOfCommits() / ((scope.latestChangeEpochDay() - scope.firstChangeEpochDay()) / 28) < 2;
+    }
+
+    private boolean hasNotEnoughAffectedLinesOfCode(ChangeScope scope) {
+        return scope.affectedLinesOfCode() <= 100;
     }
 
 }
